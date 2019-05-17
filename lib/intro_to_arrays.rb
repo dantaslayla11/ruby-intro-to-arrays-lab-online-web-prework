3cb8f2765dc46c7ac4ed954d31d28b42561b7a81
layla dantas <laylatdantas@gmail.com>
2019-05-17 23:23:45 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,25 @@
+def instantiate_new_array
+   @Array = ["Array"]
+   end
+
+def instantiate_new_array
+   @Array = []
+ end 
+  
+def array_with_two_elements
+  @Array = [1, 2]
+end 
+
+def 
+end
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
